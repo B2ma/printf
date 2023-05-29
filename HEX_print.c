348c9f6d50b24c9f000a2fcfb2b60599e322c471
B2ma <beatrice.terer@gmail.com>
2023-05-29 14:39:15 +0000

    Created HEX_print.c

---
+++ HEX_print.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+  * HEX_print - prints an hexgecimal number
+  * @val: the arguments
+  * Return: the counter
+  */
+int HEX_print(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(va, unsigned int);
+	unsigned int temp = numb;
+
+	while (numb / 16 != 0)
+	{
+		numb /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 7;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
