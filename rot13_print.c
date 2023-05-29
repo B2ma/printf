8c8be74186aba5b8f106162793728355344171d3
Chikombe <chikombe.ecy@gmail.com>
2023-05-29 06:50:37 -0700

    rot13_print.c

---
+++ rot13_print.c
@@ -0,0 +1,37 @@
+#include "main'h"
+
+/**
+ * rot13_print - prints string to ROT13 place into buffer
+ * @ap: type struct va_arg where is allocated printf arguments
+ * Return: counter
+ */
+int rot13_print(va_list ap)
+{
+	int i, j, counter = 0;
+	int k = 0;
+	char *s = va_arg(ap, char*);
+	char alpha[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char beta[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZabcdefghijklm"};
+
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i]; i++)
+	{
+		k = 0;
+		for (j = 0; alpha[j] && !k; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				_putchar(beta[j]);
+				counter++;
+				k = 1;
+			}
+		}
+		if (!k)
+		{
+			_putchar(s[i]);
+			counter++;
+		}
+	}
+	return (counter);
+}
