511a11034804f02ba8c0af7d4c09cdd3b704cfbc
B2ma <beatrice.terer@gmail.com>
2023-05-29 13:11:53 +0000

    Created dec_print.c

---
+++ dec_print.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+  * dec_print - prints decimals
+  * @ap: arguments to print
+  * Return: number of characters to be printed
+  */
+int dec_print(va_list ap)
+{
+	int n = va_arg(ap, int);
+	int numb, last = n % 10, digit;
+	int i = 1;
+	int exp = 1;
+
+	n = n / 10;
+	numb = n;
+	if (last < 0)
+	{
+		_putchar('-');
+		numb = -numb;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (numb > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			numb = numb / 10;
+		}
+		numb = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			numb = numb - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+	return (i);
+}
