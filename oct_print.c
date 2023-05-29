6e3e396adb3f9be5115ab623ed0b6aeb3081d2ad
Chikombe <chikombe.ecy@gmail.com>
2023-05-29 07:21:56 -0700

    oct_print.c

---
+++ oct_print.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * oct_print - prints an octal number
+ * @val: ap
+ * Return: counter
+ */
+
+int oct_print(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int temp = num;
+
+	while (num / 8 != 0)
+	{
+		num /= 8;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int))
+
+	for (i = 0; i < counter, i++)
+	{
+		array[i] = temp % 8
+		temp /= 8;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		_putchar(array[i] = '0');
+	}
+	free(array);
+	return (counter);
+}
