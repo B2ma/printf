a7facd2da7b5f403250adcf524391431aeab8e63
Chikombe <chikombe.ecy@gmail.com>
2023-05-29 06:18:03 -0700

    srev_print.c

---
+++ srev_print.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * srev_print - prints a string in reverse
+ * @ap: type stuct va_arg where is allocated printf arguments
+ * Return: the string
+ */
+
+int srev_print(va_list ap)
+{
+	char s = va_arg(ap, char*);
+	int i;
+	int j = 0;
+
+	if (s == NULL)
+		s = "(null)";
+	while (s[j] != '\0')
+		j++;
+	for (i = j - 1; i >= 0; i--)
+		_putchar(s[i]);
+	return (j);
+}
