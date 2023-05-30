7e7329b77d6d47a49d9441700c8659c43deb4642
B2ma <beatrice.terer@gmail.com>
2023-05-30 13:21:19 +0000

    updated _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,25 @@
 #include "main.h"
 #include <unistd.h>
 
+#define BUFFER_SIZE 1024
 /**
- * _putchar - writes the character c to stdout
- * @c: the character to print
- * Return: on success 1
- */
+  * _putchar - prints characters
+  * @c: the character to be printed
+  * Return: byte size
+  */
 
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	static char buffer[BUFFER_SIZE];
+	static int buffer_index;
+	int bytes_written = 0;
+
+	buffer[buffer_index++] = c;
+	if (buffer_index == BUFFER_SIZE)
+	{
+		bytes_written = write(1, buffer, BUFFER_SIZE);
+		buffer_index = 0;
+	}
+	return (bytes_written > 0 ? 1 : -1);
 }
+
