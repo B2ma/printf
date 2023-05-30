9ea97f7b9228b7ea65e1eea8813e64b5664c25f7
Chikombe <chikombe.ecy@gmail.com>
2023-05-30 09:06:10 -0700

    _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,17 +1,6 @@
 #include "main.h"
+#include<unistd.h>
 
-/**
- * print_buffer - prints the content of buffer
- * @buffer: array of characters
- * @buff_ind: index to add next char
- */
-
-void print_buffer(char buffer[], int *buff_ind)
-{
-	if (*buff_ind > 0)
-		write(1, buffer, *buff_ind);
-	*buff_ind = 0;
-}
 /**
   * _putchar - writes the character c to stdout
   * @c: the character to print
@@ -19,13 +8,5 @@ void print_buffer(char buffer[], int *buff_ind)
   */
 int _putchar(char c)
 {
-	static char buffer[BUFF_SIZE];
-	static int buffer_index;
-
-	if (buffer_index == BUFF_SIZE)
-	{
-		print_buffer(buffer, &buffer_index);
-	}
-	buffer[buffer_index] = c;
-	return (1);
+	return (write(1, &c, 1));
 }
